6e50bd71b59f00272b1acf052b2195af93b4c78a
Chayanon6409682538 <chayanon.kha@dome.tu.ac.th>
2025-06-04 23:38:45 +0700

    add arduino folder

--- AndroidApp/FaceRecognitionImages/openCV/build/intermediates/cxx/Debug/6p4y7726/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\PC\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\work\\study\\Year4\\Project\\andriodApp\\FaceRecognitionImagesStarter2024\\openCV\\.cxx\\Debug\\6p4y7726\\x86_64" ^
-  opencv_jni_shared
